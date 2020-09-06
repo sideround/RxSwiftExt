d3275078afcd3b092c6035ee10636044f7268f07
Isaac Rodriguez <koideapps@gmail.com>
2020-09-06 05:43:01 +0200

    Add dynamic linking support

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 let package = Package(
     name: "RxSwiftExt",
     platforms: [
-        .iOS(.v8), .tvOS(.v9), .macOS(.v10_11), .watchOS(.v3)
+        .iOS(.v10), .tvOS(.v9), .macOS(.v10_11), .watchOS(.v3)
     ],
     products: [
-        .library(name: "RxSwiftExt", targets: ["RxSwiftExt"]),
+        .library(name: "RxSwiftExt", type: .dynamic, targets: ["RxSwiftExt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift", .branch("5.1.0-spm-dynamic"))
     ],
     targets: [
         .target(name: "RxSwiftExt", dependencies: ["RxSwift", "RxCocoa"], path: "Source"),
